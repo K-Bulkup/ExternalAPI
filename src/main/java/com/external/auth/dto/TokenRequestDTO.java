a26b5d5d2eb26b1e9461a71524f10f37920c09f6
HyoSangByun <billy04100410@gmail.com>
2025-07-23 16:06:48 +0900

    Feat: request body 로 유저 계좌,은행,ID 받아오는 기능 구현

--- src/main/java/com/external/auth/dto/TokenRequestDTO.java
+++ src/main/java/com/external/auth/dto/TokenRequestDTO.java
@@ -1,5 +1,18 @@
 package com.external.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TokenRequestDTO {
-//    private String fintechUseNum;
+    private BigInteger userId;
+    private String bank;
+    private String accountNum;
 }
--- src/main/java/com/external/auth/service/AuthService.java
+++ src/main/java/com/external/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package com.external.auth.service;
 
 import com.external.auth.domain.UserVO;
 import com.external.auth.dto.TokenResponseDTO;
+import com.external.auth.exception.UnauthorizedException;
 import com.external.auth.mapper.AuthMapper;
 import com.external.auth.util.JwtUtil;
 import com.external.auth.util.RefreshUtil;
@@ -31,27 +32,39 @@ public class AuthService {
     private RedisUtil redisUtil;
 
     @Transactional
-    public TokenResponseDTO createFintechUseNum() {
+    public TokenResponseDTO createFintechUseNum(BigInteger userId, String bank, String accountNum) {
         String fintechUseNum = UUID.randomUUID().toString();
 
-        UserVO userVO = UserVO.of(
-                BigInteger.valueOf(1),
-                "111111-222222",
-                "국민은행",
-                fintechUseNum
-        );
+        UserVO userVO = UserVO.of(userId, accountNum, bank, fintechUseNum);
         authMapper.createUser(userVO);
-        String refreshToken = createRefreshToken(fintechUseNum);
-        String accessToken = jwtUtil.generateToken(fintechUseNum);
+        String refreshToken = createRefreshToken(userId);
+        String accessToken = jwtUtil.generateToken(userId);
 
         return new TokenResponseDTO(accessToken, refreshToken, fintechUseNum);
     }
 
-    public String createRefreshToken(String fintechUseNum) {
+    private String createRefreshToken(BigInteger userId) {
         String refreshToken = refreshUtil.generateRefreshToken();
-        redisUtil.setValue(fintechUseNum, refreshToken, expireDuration);
-
+        redisUtil.setValue("refresh:user:" + userId, refreshToken, expireDuration);
         return refreshToken;
     }
+
+    @Transactional
+    public TokenResponseDTO validateAndGetUser(String accessToken) {
+        if (!jwtUtil.validateToken(accessToken)) {
+            throw new UnauthorizedException("토큰이 유효하지 않습니다.");
+        }
+        BigInteger userId = jwtUtil.getUserId(accessToken);
+        UserVO user = authMapper.findByUserId(userId);
+
+        if(user == null) {
+            throw new UnauthorizedException("유효하지 않은 핀테크 이용번호입니다.");
+        }
+
+        String refreshToken = redisUtil.getValue("refresh:user:" + user.getUserId());
+        String fintechUseNum = authMapper.findFintechUseNumByUserId(userId);
+        return new TokenResponseDTO(accessToken, refreshToken, fintechUseNum);
+    }
+
 }
 
